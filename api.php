95765834266783fd917e610d8021af0542cc86c1
Yue <zhikuan0322@gmail.com>
2022-03-17 18:24:59 +0800

    new

--- api.php
+++ api.php
@@ -1,15 +1,14 @@
 <?php
-
 $pdo = new PDO("mysql:dbname=web52;host=localhost;port=3306", "admin", "1234");
 $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
 
 $sql = "INSERT INTO tickets(`firstname`, `lastname`, `phonenumber`, `password`) VALUES (?, ?, ?, ?)";
 $stmt = $pdo->prepare($sql);
-$stmt->execute(array($_POST["firstname"], $_POST["lastname"], $_POST["phonenumber"], $_POST["pw"]));
+$stmt->execute(array($_POST["firstname"], $_POST["lastname"], $_POST["phone"], $_POST["pw"]));
 
 echo "
     <script>
-    alert('訂票成功');
-    window.location.href = '/';
+        alert('Buy Ticket Success');
+        window.location.href = '/';
     </script>
 ";
